4f428e315bc274e69acf02fb50f859d848f070b8
Saintdannyyy <danieltesla746@gmail.com>
2025-05-11 16:10:10 +0000

    rename main.py

--- app.py
+++ app.py
@@ -329,4 +329,4 @@ async def supported_languages():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
